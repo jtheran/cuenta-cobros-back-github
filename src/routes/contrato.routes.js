d4b2768deb9797d044950c805f6145f0acb147cd
Daniel florez <danieldu20@hotmail.com>
2025-05-29 00:31:15 -0500

    errores de sintaxis

--- src/routes/contrato.routes.js
+++ src/routes/contrato.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { authenticateToken, authorizeRoles } from '../middlewares/auth.js'; 
+import { authenticateToken, authorizeRoles } from '../middlewares/auth.js';
 import { getContrats, getContractByID, createContract, deleteContrac, updateContract } from '../controllers/contrato.controller.js';
 import upload from '../libs/multer.js';
 
@@ -7,9 +7,9 @@ import upload from '../libs/multer.js';
 const router = Router();
 
 // Rutas protegidas con authenticateToken y authorizeRoles
-router.get('/contract', authenticateToken, authorizeRoles(['admin', 'contrastista']), getContrats);
+router.get('/contract', authenticateToken, authorizeRoles(['admin', 'contratista']), getContrats);
 
-router.get('/contract/:id', authenticateToken, authorizeRoles(['admin', 'contrastista']), getContractByID);
+router.get('/contract/:id', authenticateToken, authorizeRoles(['admin', 'contratista']), getContractByID);
 
 router.post('/contract', authenticateToken, authorizeRoles(['admin']), createContract);
 
