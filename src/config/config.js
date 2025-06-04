0c0ffe3da4420fdbee2dad4a9db7fc5f15a82d51
JMTB <jtheran@lsv-tech.com>
2025-06-04 13:48:06 -0500

    FIx: valores de enviroment

--- src/config/config.js
+++ src/config/config.js
@@ -3,15 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-    port: process.env.PORT || 2048,
-    key: process.env.KEY_SECRET || 'zaqwer',
-    refreshKey: process.env.REFRESH_KEY || 'polik',
-    hostMail: process.env.MAIL_HOST || '',
-    portMail: process.env.MAIL_PORT || '456',
+    port: process.env.PORT || 3325,
+    key: process.env.KEY_SECRET || 'qwerty',
+    refreshKey: process.env.REFRESH_KEY || 'qazwer',
+    hostMail: process.env.MAIL_HOST || 'server1.lsv-tech.com',
+    portMail: process.env.MAIL_PORT || 456,
     salt: process.env.SALT || 10,
-    adminEmail: process.env.EMAIL_ADMIN || 'admin@admin.com',
-    passAdminEmail: process.env.PASS_EMAIL_ADMIN || 'Testing25!',
-    passAdmin: process.env.PASS_ADMIN || 'Testing25!',
+    adminEmail: process.env.EMAIL_ADMIN || 'qa@lsv-tech.com',
+    passAdminEmail: process.env.PASS_EMAIL_ADMIN || "o].tGf)%=$GK",
+    passAdmin: process.env.PASS_ADMIN || 'Testing24@',
 }
 
 
